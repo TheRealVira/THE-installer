c5a5bc2175a7193f61f7707c351a62915bb9a3ae
TheRealVira <tom.ruehrig@aon.at>
2017-01-05 12:49:38 +0100

    Fixed github file management.

---
+++ Installer/HashManager.cs
@@ -0,0 +1,128 @@
+﻿#region License
+
+// Copyright (c) 2017, Vira
+// All rights reserved.
+// Solution: Installer
+// Project: Installer
+// Filename: HashManager.cs
+// Date - created:2017.01.05 - 09:04
+// Date - current: 2017.01.05 - 12:43
+
+#endregion
+
+#region Usings
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+
+#endregion
+
+namespace Installer
+{
+    internal static class HashManager
+    {
+        private static readonly XmlSerializer Serializer = new XmlSerializer(typeof(Item[]),
+            new XmlRootAttribute {ElementName = "items"});
+
+        /// <summary>
+        ///     Returns the MD5 hash for a direcotry. (http://stackoverflow.com/a/15683147)
+        /// </summary>
+        /// <param name="dir">The path (folder) from which the hash should get generated.</param>
+        /// <returns>Returns a new generated MD5 hash for a directory.</returns>
+        public static byte[] CreateHash(string dir)
+        {
+            // assuming you want to include nested folders
+            var files = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories)
+                .OrderBy(p => p).ToList();
+
+            var md5 = MD5.Create();
+            var hashes = new int[files.Count];
+
+            for (var i = 0; i < files.Count; i++)
+            {
+                hashes[i] = CreateHashForFile(files[i], md5).GetHashCode();
+                //var file = files[i];
+
+                //// hash path
+                //var relativePath = file.Substring(dir.Length + 1);
+                //var pathBytes = Encoding.UTF8.GetBytes(relativePath.ToLower());
+                //md5.TransformBlock(pathBytes, 0, pathBytes.Length, pathBytes, 0);
+
+                //// hash contents
+                //var contentBytes = File.ReadAllBytes(file);
+                //if (i == files.Count - 1)
+                //{
+                //    md5.TransformFinalBlock(contentBytes, 0, contentBytes.Length);
+                //}
+                //else
+                //{
+                //    md5.TransformBlock(contentBytes, 0, contentBytes.Length, contentBytes, 0);
+                //}
+            }
+
+
+            return md5.Hash;
+        }
+
+        /// <summary>
+        ///     Creates the MD5 hash for a single file.
+        /// </summary>
+        /// <param name="file">The source file for the MD5 hash.</param>
+        /// <returns>Returns the generated MD5 hash-</returns>
+        public static byte[] CreateHashForFile(string file, MD5 md5 = null)
+        {
+            md5 = md5 ?? MD5.Create(); // Create a new one if there is no one given over parameter.
+
+            using (var reader = File.OpenRead(file))
+            {
+                md5.ComputeHash(reader);
+
+                reader.Flush();
+                reader.Close();
+                reader.Dispose();
+            }
+
+            return md5.Hash;
+        }
+
+        /// <summary>
+        ///     This will save the your byte array into a file.
+        /// </summary>
+        /// <param name="data">Your data that you want to save inside that file.</param>
+        /// <param name="destination">The destination file.</param>
+        public static void Save(this Dictionary<string, byte[]> data, string destination)
+        {
+            using (var writer = new StreamWriter(destination))
+            {
+                Serializer.Serialize(writer, data.Select(x => new Item {id = x.Key, value = x.Value}).ToArray());
+
+                writer.Flush();
+                writer.Close();
+                writer.Dispose();
+            }
+        }
+
+        /// <summary>
+        ///     Loads your byte array from a given file (destination).
+        /// </summary>
+        /// <param name="destination">The file destination.</param>
+        /// <returns>Returns your loaded byte array.</returns>
+        public static Dictionary<string, byte[]> Load(this string destination)
+        {
+            Dictionary<string, byte[]> toRet;
+            using (var reader = new StreamReader(destination))
+            {
+                toRet = ((Item[]) Serializer.Deserialize(reader))
+                    .ToDictionary(i => i.id, i => i.value);
+
+                reader.Close();
+                reader.Dispose();
+            }
+
+            return toRet;
+        }
+    }
+}
---
+++ Installer/Properties/AssemblyInfo.cs
@@ -0,0 +1,55 @@
+﻿#region License
+
+// Copyright (c) 2017, Vira
+// All rights reserved.
+// Solution: Installer
+// Project: Installer
+// Filename: AssemblyInfo.cs
+// Date - created:2017.01.05 - 09:04
+// Date - current: 2017.01.05 - 12:43
+
+#endregion
+
+#region Usings
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Installer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Installer")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("f155e187-aa6c-40aa-880c-21b3ff00c1a8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
